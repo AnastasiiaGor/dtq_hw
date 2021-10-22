e4d4c7d18fe59e7320fa7f94ee36c407b3905144
Anastasiia_Goriachki <anastasiia_goriachkina@epam.com>
2021-10-22 14:29:08 +0300

    Part2 fix

--- HW_2/countryRank.sql
+++ HW_2/countryRank.sql
@@ -1,16 +1,13 @@
-CREATE TABLE countryRank AS 
-  SELECT DISTINCT cont.name AS continent, r.name AS region, cn.area, cn.name AS name, cn.country_code3, 
-                 CASE WHEN cn.national_day IS NULL THEN 'no data' 
-                      WHEN cn.national_day > CURRENT_DATE THEN CURRENT_DATE 
-                      ELSE cn.national_day END AS nday 
-  FROM regions r JOIN region_areas ra ON r.name = ra.region_name 
-                 JOIN countries cn ON r.region_id = cn.region_id 
-                 JOIN (
-    SELECT c.*, ca.cont_area from continents c JOIN (
-      SELECT r.continent_id, SUM(ra.region_area) AS cont_area 
-      FROM regions r JOIN region_areas ra ON r.name = ra.region_name 
-      GROUP BY r.continent_id) ca 
-    ON c.continent_id = ca.continent_id) cont 
-  ON r.continent_id = cont.continent_id 
-  ORDER BY cont.cont_area DESC, ra.region_area DESC, cn.area DESC;
-ALTER TABLE countryRank ADD COLUMN rank INT AUTO_INCREMENT UNIQUE FIRST;
+CREATE TABLE countryRank AS
+	SELECT ranked.rank, c.name AS continent, ranked.region_name, ranked.name, ranked.area, ranked.country_code3, ranked.nday 
+	FROM continents c JOIN (
+    	SELECT *, RANK() OVER(PARTITION BY rc.region_name ORDER BY rc.area DESC) rank 
+    	FROM (
+        	SELECT DISTINCT r.name AS region_name, cn.name, cn.area, cn.country_code3, 
+               CASE WHEN cn.national_day IS NULL THEN 'no data' 
+                    WHEN cn.national_day > CURRENT_DATE THEN CURRENT_DATE                
+                    ELSE cn.national_day END AS nday, r.continent_id 
+        	FROM regions r JOIN region_areas ra ON r.name = ra.region_name 
+                       	   JOIN countries cn ON cn.region_id = r.region_id 
+    	ORDER BY cn.area DESC) rc) ranked ON c.continent_id = ranked.continent_id 
+	ORDER BY c.name, ranked.region_name, ranked.area DESC;
